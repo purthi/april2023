9d0cdbc7b120da7f1a7f3151e541be6a44f073c7
root <root@ip-172-31-8-45.ap-northeast-1.compute.internal>
2023-05-01 05:26:47 +0000

    added a loop

--- file.sh
+++ file.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-echo "Enter your name\n"
+echo
+echo "Enter your name"
+echo
 read name
-echo "Hello $name! Welcome to the session\n"
+echo "Hello $name! Welcome to the session"
+echo
+#including a loop in the script
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+  if [ $i == 2 ]
+then
+    break
+  fi
+#print the value
+echo "Congratulations $i"
+echo
+done
